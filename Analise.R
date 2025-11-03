d6926783bcf525717e62343dd2c8e902b62cc69d
Isabelle Salvador <isabelle_salvador@id.uff.br>
2025-11-03 10:06:37 -0300

    Incluindo o cálculo da correlação

--- Analise.R
+++ Analise.R
@@ -1,5 +1,11 @@
-#Criando um vetor
+#Criando um vetor x
 x = c(2, 6, 8, 10)
 
-#calculando a média dos valores de x
+#Calculando a média dos valores de x
 mean(x = x, na.rm = TRUE)
+
+#Criando um vetor y
+y = c(3, 7, 6, 12)
+
+#Calculando a correlação
+cor(x = x, y = y)
